1687f1c84da694e4730e4e34f5e09e258917ea09
mjeensung <mjtwins89@gmail.com>
2025-03-06 10:11:07 +0900

    1603

---
+++ Week1/leetcode_1603.py
@@ -0,0 +1,6 @@
+#https://leetcode.com/problems/design-parking-system/
+
+class ParkingSystem:
+    def __init__(self, big: int, medium: int, small: int):
+
+    def addCar(self, carType: int) -> bool:
