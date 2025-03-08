d9f2f6b01e8099a557b51b2adfe747e5e2d96394
HyunwooKim04 <155235090+HyunwooKim04@users.noreply.github.com>
2025-03-08 16:57:22 +0900

    Update leetcode_1365.py

--- Week1/leetcode_1365.py
+++ Week1/leetcode_1365.py
@@ -4,4 +4,11 @@ from typing import List
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-    
+        sorted_nums = sorted((num, i) for i, num in enumerate(nums))
+        result = [0] * len(nums)
+
+        for i in range(len(nums)):
+            if i > 0 and sorted_nums[i][0] > sorted_nums[i-1][0]:
+                result[sorted_nums[i][1]] = i
+        
+        return result
